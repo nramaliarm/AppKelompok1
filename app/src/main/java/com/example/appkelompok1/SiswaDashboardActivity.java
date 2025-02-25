065177627e3c0d967091f912699f4663c63c913c
alyaa <nuramaliaramadhanii30@gmail.com>
2025-02-25 20:56:51 +0800

    update

--- app/src/main/java/com/example/appkelompok1/SiswaDashboardActivity.java
+++ app/src/main/java/com/example/appkelompok1/SiswaDashboardActivity.java
@@ -1,11 +1,9 @@
 package com.example.appkelompok1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SiswaDashboardActivity extends AppCompatActivity {
 
@@ -13,29 +11,34 @@ public class SiswaDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siswa_dashboard);
-    }
 
-    // Inisialisasi BottomNavigationView
-    BottomNavigationItemView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // Inisialisasi BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-   (R.id.fragment_container, new SiswaDashboardFragment()).commit();
-
-        bottomNavigationView.setOn(item -> {Fragment selectedFragment = null;
-
-        if (item.getItemId() == R.id.home) {
-            selectedFragment = new SiswaDashboardFragment();
-        } else if (item.getItemId() == R.id.school) {
-            selectedFragment = new SchoolFragment();
-        } else if (item.getItemId() == R.id.formulir) {
-            selectedFragment = new FormulirFragment();
-        } else if (item.getItemId() == R.id.akun) {
-            selectedFragment = new AkunFragment();
-        }
-
-        // Ganti fragment
+        // Set default fragment saat pertama kali dibuka
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment).commit();
+                .replace(R.id.fragment_container, new SiswaDashboardFragment()).commit();
 
-        return true;
-    });
+        // Set listener untuk item bottom navigation
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            // Tentukan fragment berdasarkan item yang dipilih
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new SiswaDashboardFragment();
+            } else if (item.getItemId() == R.id.school) {
+                selectedFragment = new SchoolFragment();
+            } else if (item.getItemId() == R.id.formulir) {
+                selectedFragment = new FormulirFragment();
+            } else if (item.getItemId() == R.id.akun) {
+                selectedFragment = new AkunFragment();
+            }
+
+            // Ganti fragment yang dipilih
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        });
+    }
 }
